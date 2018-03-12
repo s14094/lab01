74fe5fe9fd233428c03fd82985398fa84fbf29e2
Kickd <s14094@pjwstk.edu.pl>
2018-03-12 10:30:41 +0100

    new tests

---
+++ src/main/java/pl/pawellakomiec/domain/System.java
@@ -0,0 +1,16 @@
+package pl.pawellakomiec.domain;
+
+import java.util.*;
+
+public class System{
+
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
