063146ef6a060ac07fd9799b56f1503745a25888
Kickd <s14094@pjwstk.edu.pl>
2018-04-22 21:10:16 +0200

    other version of mockito

--- src/test/java/pl/pawellakomiec/repository/TransmiterRepositoryTestMockito.java
+++ src/test/java/pl/pawellakomiec/repository/TransmiterRepositoryTestMockito.java
@@ -8,7 +8,8 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+
+import org.mockito.junit.MockitoJUnitRunner;
 import pl.pawellakomiec.domain.Transmiter;
 
 import java.sql.Connection;
