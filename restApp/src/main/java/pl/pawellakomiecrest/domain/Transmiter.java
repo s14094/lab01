17c5cca59c601c745534a0a836f920b02d3a9fe4
Kickd <s14094@pjwstk.edu.pl>
2018-04-22 20:35:17 +0200

    rest app

---
+++ restApp/src/main/java/pl/pawellakomiecrest/domain/Transmiter.java
@@ -0,0 +1,42 @@
+package pl.pawellakomiecrest.domain;
+
+public class Transmiter {
+
+    private int id;
+    private String name;
+    private int price;
+    private int power;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+}
---
+++ restApp/src/main/java/pl/pawellakomiecrest/service/TransmiterRepository.java
@@ -0,0 +1,21 @@
+package pl.pawellakomiecrest.service;
+
+
+import pl.pawellakomiecrest.domain.Transmiter;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface TransmiterRepository {
+
+    List<Transmiter> getAll();
+    Transmiter getById(int id) throws SQLException;
+    int addTransmiter(Transmiter transmiter);
+    int deleteTransmiter(Transmiter transmiter) throws SQLException;
+    void updateTransmiter(int oldId, Transmiter newTransmiter) throws SQLException;
+    void dropDatatable() throws SQLException;
+    void setConnection(Connection connection) throws SQLException;
+    Connection getConnection();
+
+}
