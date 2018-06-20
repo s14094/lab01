d5995cb7341b8dc1536eb9c702a8eaf105655a79
Kickd <pawel.lakomiec@dahliamatic.pl>
2018-06-20 15:51:50 +0200

    catch2 = v2

--- wyklad12-catch2/example1/tests/test_lib.cpp
+++ wyklad12-catch2/example1/tests/test_lib.cpp
@@ -2,11 +2,24 @@
 
 #include <rpn.hpp>
 
-using Catch::Matchers::EndsWith; // see "matchers"
+//using Catch::Matchers::EndsWith; // see "matchers"
+using Catch::Matchers::Contains; // see "matchers"
+
 
 TEST_CASE("Some test 2")
 {
     REQUIRE((10 + 2) == 12);
+    rpn testt = rpn();
+    REQUIRE(testt.count("14094 10 +") == 14104);
+     REQUIRE(testt.count("14094 10 -") == 14084);
+     // (1 + 5 - 2) * 3 / 6 
+     REQUIRE(testt.count("1 5 + 2 - 3 * 6 /") == 2);
+}
+
+TEST_CASE("Error handling")
+{
+    rpn test = rpn();
+    REQUIRE_THROWS_WITH(test.count("10 2 %"), Contains("%"));
 }
 
 SCENARIO("Simple scenario")
