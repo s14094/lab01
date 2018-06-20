536553712d617396a7811e0a23a9511ea35d5d10
Kickd <pawel.lakomiec@dahliamatic.pl>
2018-06-20 14:02:38 +0200

    selenium v5

--- src/test/java/pl/pawellakomiec/domain/selenium/SiteTest.java
+++ src/test/java/pl/pawellakomiec/domain/selenium/SiteTest.java
@@ -1,4 +1,4 @@
-package pl.pawellakomiec.domain;
+package pl.pawellakomiec.domain.selenium;
 
         import static org.junit.Assert.assertEquals;
         import static org.junit.Assert.assertNotNull;
